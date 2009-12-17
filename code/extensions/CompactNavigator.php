42631e08f5419beb011ff193efe5eff3a1ae3c94
Morven Lewis-Everley <morven@i-lateral.com>
2009-12-17 11:29:15 +0000

    Add ability to add custom CSS/JavaScript or templates.

--- code/extensions/CompactNavigator.php
+++ code/extensions/CompactNavigator.php
@@ -21,12 +21,51 @@ class CompactNavigator extends Extension {
 	/**
 	 * @var $adminLink Link to the main admin area
 	 */
-	static $adminLink = 'admin';
+	static $adminLink	= 'admin';
+	
+	/**
+	 * Setting the Template static will allow you to use your own custom template files,
+	 * that overwrite the default behaviour.
+	 * 
+	 * @todo Ensure that just adding CompactNavigator.ss to your theme will automatically
+	 * overwrite the default
+	 * 
+	 * @var String of the Name of your template (no .ss)
+	 */
+	public static $Template;
+	/**
+	 * Setting CssTheme allows you to add a custom CSS file, instead of using the default one.
+	 * Please note: using this will load your CSS file, instead of the default.
+	 * 
+	 * @var relative path to CSS file from site root
+	 */
+	public static $CssTheme;
+	/**
+	 * Setting JsTheme allows you to add a custom JavaScript file, instead of using the default one.
+	 * Please note: using this will load your JavaScript file, instead of the default.
+	 * 
+	 * @var relative path to JavaScript file from site root
+	 */
+	public static $JsTheme;
 
+	/**
+	 * SSCompactNavigator first checks if you are allowed to see the navigation bar, and if so, then checks
+	 * if third party templates have been specified. If so, it loads them, and provides them with the required
+	 * variables. If not, it loads the defaults instead.
+	 */
 	public function SSCompactNavigator() {
 		if(Director::isDev() || Permission::check('CMS_ACCESS_CMSMain')) {
-			Requirements::css('compactnavigator/css/CompactNavigator.css');
-			Requirements::javascript('compactnavigator/scripts/CompactNavigator.js');
+			$RenderTemplate = (isset(CompactNavigator::$Template)) ? CompactNavigator::$Template : $this->class;
+			
+			if(isset(CompactNavigator::$CssTheme))
+				Requirements::css(CompactNavigator::$CssTheme);
+			else
+				Requirements::css('compactnavigator/css/CompactNavigator.css');
+
+			if(isset(CompactNavigator::$JsTheme))
+				Requirements::javascript(CompactNavigator::$JsTheme);
+			else
+				Requirements::javascript('compactnavigator/scripts/CompactNavigator.js');
 
 			$this->owner->cmsLink	= self::$adminLink."/".CMSMain::$url_segment."/show";
 			$this->owner->adminLink	= self::$adminLink;
@@ -37,7 +76,10 @@ class CompactNavigator extends Extension {
 			} else
 				$this->owner->DisplayMode = Versioned::current_stage();
 
-			return $this->owner->renderWith('CompactNavigator');
+			return $this->owner->renderWith(array(
+				$RenderTemplate,
+				'CompactNavigatior'
+			));
 		}
 	}
 }
